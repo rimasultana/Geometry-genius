cc11b58bc5ecd10c4b8a17bb4cca5ff699489c79
rimasultana <rimasultana20225@gmail.com>
2024-09-20 15:50:14 +0600

    done card-1

--- index.js
+++ index.js
@@ -3,19 +3,72 @@ document.getElementById("triangle").addEventListener("click", function () {
   serial++;
   const inputB = getInputValue("traingle-b");
   const inputH = getInputValue("traingle-h");
-  const traingle = getHtmlInnerText("traingle-title");
+  const traingleTitle = getHtmlInnerText("traingle-title");
 
   const sum = 0.5 * inputB * inputH;
-  const tableCantainer = document.getElementById("table-container");
+  const tableContainer = document.getElementById("table-container");
   const tableRow = document.createElement("tr");
   tableRow.innerHTML = `
     <td>${serial}</td>
-    <td>${traingle}</td>
-    <td>${sum}cm <sup>2</sup></td>
-    <td><button class="btn bg-lime-400">convert to cm</button></td>
+    <td>${traingleTitle}</td>
+    <td>${sum}cm<sup>2</sup></td>
+    <td><button class="btn bg-lime-400">convert to m<sup>2</sup></button></td>
   
   `;
-  tableCantainer.appendChild(tableRow);
+  tableContainer.appendChild(tableRow);
+});
+
+document.getElementById("rectangle").addEventListener("click", function () {
+  serial++;
+  const inputW = getInputValue("rectangle-w");
+  const inputI = getInputValue("rectangle-i");
+  const rectangleTitle = getHtmlInnerText("rectangle-title");
+
+  const sum = inputW * inputI;
+
+  const tableContainer = document.getElementById("table-container");
+  const tableRow = document.createElement("tr");
+  tableRow.innerHTML = `
+    <td>${serial}</td>
+    <td>${rectangleTitle}</td>
+    <td>${sum}cm<sup>2</sup></td>
+    <td><button class="btn bg-lime-400">convert to m<sup>2</sup></button></td>
+  `;
+  tableContainer.appendChild(tableRow);
+});
+
+document.getElementById("parallelogram").addEventListener("click", function () {
+  serial++;
+  const parallelogramInputB = getInputValue("parallelogram-b");
+  const parallelogramInputH = getInputValue("parallelogram-h");
+  const parallelogramTitle = getHtmlInnerText("parallelogram-title");
+  const sum = parallelogramInputB * parallelogramInputH;
+
+  const tableContainer = document.getElementById("table-container");
+  const tableRow = document.createElement("tr");
+  tableRow.innerHTML = `
+  <td>${serial}</td>
+  <td>${parallelogramTitle}</td>
+  <td>${sum}cm<sup>2</sup></td>
+      <td><button class="btn bg-lime-400">convert to m<sup>2</sup></button></td>`;
+  tableContainer.appendChild(tableRow);
+});
+
+document.getElementById("rhombus").addEventListener("click", function () {
+  serial++;
+  const rhombusInputd1 = getInputValue("rhombus-d1");
+  const rhombusInputd2 = getInputValue("rhombus-d2");
+  const rhombusTitile = getHtmlInnerText("rhombus-title");
+
+  const sum = 0.5 * rhombusInputd1 * rhombusInputd2;
+  const tableContainer = document.getElementById("table-container");
+  const tableRow = document.createElement("tr");
+  tableRow.innerHTML = `
+  <td>${serial}</td>
+  <td>${rhombusTitile}</td>
+  <td>${sum}cm<sup>2</sup></td>
+      <td><button class="btn bg-lime-400">convert to m<sup>2</sup></button></td>`;
+  tableContainer.appendChild(tableRow);
 });
 
 function getInputValue(id) {
@@ -27,11 +80,3 @@ function getHtmlInnerText(id) {
   const innerText = document.getElementById(id).innerText;
   return innerText;
 }
-
-let input = document.getElementById("userInput");
-input.value.text = "New Text";
-console.log(input.value.text = "New Text");
-
-document.getElementById("nonExistentButton").addEventListener("click", function() {
-    alert("Clicked");
-  });
