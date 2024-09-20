768138447413c1d9b3424d150792d7716d89370e
rimasultana <rimasultana20225@gmail.com>
2024-09-20 16:55:44 +0600

    done

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 let serial = 0;
+// card-1
 document.getElementById("triangle").addEventListener("click", function () {
   serial++;
   const inputB = getInputValue("traingle-b");
@@ -17,7 +18,7 @@ document.getElementById("triangle").addEventListener("click", function () {
   `;
   tableContainer.appendChild(tableRow);
 });
-
+// card-2
 document.getElementById("rectangle").addEventListener("click", function () {
   serial++;
   const inputW = getInputValue("rectangle-w");
@@ -36,11 +37,11 @@ document.getElementById("rectangle").addEventListener("click", function () {
   `;
   tableContainer.appendChild(tableRow);
 });
-
+// card-3
 document.getElementById("parallelogram").addEventListener("click", function () {
   serial++;
-  const parallelogramInputB = getInputValue("parallelogram-b");
-  const parallelogramInputH = getInputValue("parallelogram-h");
+  const parallelogramInputB = getHtmlInnerText("parallelogram-b");
+  const parallelogramInputH = getHtmlInnerText("parallelogram-h");
   const parallelogramTitle = getHtmlInnerText("parallelogram-title");
   const sum = parallelogramInputB * parallelogramInputH;
 
@@ -53,11 +54,12 @@ document.getElementById("parallelogram").addEventListener("click", function () {
       <td><button class="btn bg-lime-400">convert to m<sup>2</sup></button></td>`;
   tableContainer.appendChild(tableRow);
 });
+// card-4
 
 document.getElementById("rhombus").addEventListener("click", function () {
   serial++;
-  const rhombusInputd1 = getInputValue("rhombus-d1");
-  const rhombusInputd2 = getInputValue("rhombus-d2");
+  const rhombusInputd1 = getHtmlInnerText("rhombus-d1");
+  const rhombusInputd2 = getHtmlInnerText("rhombus-d2");
   const rhombusTitile = getHtmlInnerText("rhombus-title");
 
   const sum = 0.5 * rhombusInputd1 * rhombusInputd2;
@@ -70,6 +72,41 @@ document.getElementById("rhombus").addEventListener("click", function () {
       <td><button class="btn bg-lime-400">convert to m<sup>2</sup></button></td>`;
   tableContainer.appendChild(tableRow);
 });
+// card-5
+document.getElementById("pentagon").addEventListener("click", function () {
+  serial++;
+  const pentagonInputP = getHtmlInnerText("pentagon-p");
+  const pentagonInputB = getHtmlInnerText("pentagon-b");
+  const pentagonTitle = getHtmlInnerText("pentagon-title");
+  const sum = 0.5 * pentagonInputP * pentagonInputB;
+
+  const tableContainer = document.getElementById("table-container");
+  const tableRow = document.createElement("tr");
+  tableRow.innerHTML = `
+  <td>${serial}</td>
+  <td>${pentagonTitle}</td>
+  <td>${sum}cm<sup>2</sup></td>
+  <td><button class="btn bg-lime-400">convert to m<sup>2</sup></button></td>`;
+  tableContainer.appendChild(tableRow);
+});
+// car-6
+document.getElementById('ellipse').addEventListener('click', function(){
+  serial++;
+  const ellipseInputA = getHtmlInnerText('ellipse-a');
+  const ellipseInputB = getHtmlInnerText('ellipse-b');
+  const ellipseTitle = getHtmlInnerText('ellipse-title');
+  const sum =  3.14159 * ellipseInputA * ellipseInputB;
+
+
+  const tableContainer = document.getElementById("table-container");
+  const tableRow = document.createElement("tr");
+  tableRow.innerHTML = `
+  <td>${serial}</td>
+  <td>${ellipseTitle}</td>
+  <td>${sum}cm<sup>2</sup></td>
+  <td><button class="btn bg-lime-400">convert to m<sup>2</sup></button></td>`;
+  tableContainer.appendChild(tableRow);
+})
 
 function getInputValue(id) {
   const value = Number(document.getElementById(id).value);
